f073ad91d1929e85ee79c8c48b501a50c48392ba
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2023-01-17 20:35:28 +0200

    file upload

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import fileUpload from 'express-fileupload';
 import { commentsController } from './controller/comments';
 
 const app = express();
@@ -7,6 +8,7 @@ const router = express.Router();
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 app.use('/data', router);
 
 router.get('/', commentsController.getComments);
