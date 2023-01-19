761cbcfdcb56687645f9fca4cac7c2655be2b7f6
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2023-01-19 17:12:26 +0200

    api doc

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use('/data', router);
 
 app.get('/v1', (req, res) => {
-  res.sendFile('../pubic/index.html')
+  res.sendFile('/pubic/index.html')
 })
 router.get('/', commentsController.getComments);
 router.get('/captcha', commentsController.getCaptcha);
