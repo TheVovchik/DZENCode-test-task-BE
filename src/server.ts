cb975eecbd69a86782fbdb837df727811d8035ab
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2023-01-19 18:13:19 +0200

    static

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,9 @@ const router = express.Router();
 app.use(cors());
 app.use(express.json());
 app.use('/v1/data', router);
+app.use('/v1/static', express.static('public'))
 
-app.get('/v1', (req, res) => {
+app.get('/v1/static', (req, res) => {
   res.sendFile('/pubic/index.html')
 })
 router.get('/', commentsController.getComments);
